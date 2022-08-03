0b28671b476450b54fdfec484dfe6de132f48388
lukegjpotter <lukegjpotter@gmail.com>
2022-08-03 13:11:28 +0100

    Adding Spring Boot code.

---
+++ src/main/java/com/lukegjpotter/tools/fantasycyclingleaguetools/FantasyCyclingLeagueToolsApplication.java
@@ -0,0 +1,13 @@
+package com.lukegjpotter.tools.fantasycyclingleaguetools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyCyclingLeagueToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantasyCyclingLeagueToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lukegjpotter/tools/fantasycyclingleaguetools/FantasyCyclingLeagueToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukegjpotter.tools.fantasycyclingleaguetools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyCyclingLeagueToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
