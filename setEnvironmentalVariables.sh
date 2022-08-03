8d60703b76c50d5b5b68ebf12a4d07a335ad2ead
lukegjpotter <lukegjpotter@gmail.com>
2022-08-03 15:49:12 +0100

    Adding Environmental Variables scripts

---
+++ setEnvironmentalVariables.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Test Colours
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+# Ensure that file is being sourced, as ./ won't set the Env Variables outside this script's context.
+if [ "$0" = "./setEnvironmentalVariables.sh" ]
+then
+  echo " ${red}[error]  Usage of this file should be 'source setEnvironmentalVariables.sh'${reset}"
+  exit 1
+fi
+
+# Set ROADCC_USERNAME for local testing
+echo " [info] Setting ROADCC_USERNAME"
+export ROADCC_USERNAME='yourusername'
+echo " ${green}[info] Set ROADCC_USERNAME to $ROADCC_USERNAME ${reset}"
+
+# Set ROADCC_PASSWORD for local testing
+echo " [info] Setting ROADCC_PASSWORD"
+export ROADCC_PASSWORD='yourpassword'
+echo " ${green}[info] Set ROADCC_PASSWORD to $ROADCC_PASSWORD ${reset}"
--- src/main/java/com/lukegjpotter/tools/fantasycyclingleaguetools/componentsfortransferservice/TransferSeleniumComponent.java
+++ src/main/java/com/lukegjpotter/tools/fantasycyclingleaguetools/componentsfortransferservice/TransferSeleniumComponent.java
@@ -7,7 +7,7 @@ public class TransferSeleniumComponent {
 
     public String getTransfers() {
 
-        // Login using Environmental Variables
+        // Login using Environmental Variables: ROADCC_USERNAME & ROADCC_PASSWORD
         // Select Competition - Giro, Tour, Vuelta.
         // Determine the Latest Stage
         // View League
